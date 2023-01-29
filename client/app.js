abc9e23c1d59365949ba1b4355e89a7af75808c7
TranDangKhoi <baits16@gmail.com>
2023-01-29 17:21:10 +0700

    :100: Update a button for refreshing token manually

---
+++ client/app.js
@@ -0,0 +1,107 @@
+class Http {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: "http://localhost:4000",
+      timeout: 10000,
+    });
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log(config);
+        const access_token = localStorage.getItem("access_token");
+        if (access_token) {
+          config.headers.Authorization = `Bearer ${access_token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+    this.instance.interceptors.response.use(
+      (config) => config,
+      (error) => Promise.reject(error)
+    );
+  }
+  get(url) {
+    return this.instance.get(url);
+  }
+
+  post(url, body) {
+    return this.instance.post(url, body);
+  }
+}
+
+const http = new Http();
+
+const form = document.querySelector("#login-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  http
+    .post("/login", {
+      username,
+      password,
+    })
+    .then((res) => {
+      console.log(res);
+      localStorage.setItem("access_token", res.data.data.access_token);
+      localStorage.setItem("refresh_token", res.data.data.refresh_token);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+const getProfileBtn = document.querySelector("#btn-get-profile");
+const getProductsBtn = document.querySelector("#btn-get-products");
+const refreshTokenBtn = document.querySelector("#btn-refresh-token");
+
+function fetchProfile() {
+  http
+    .get("/profile")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function fetchProducts() {
+  http
+    .get("/products")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+async function refreshToken() {
+  const refresh_token = localStorage.getItem("refresh_token");
+  try {
+    const res = await http.post("refresh-token", {
+      refresh_token: refresh_token,
+    });
+    const { access_token } = res.data.data;
+    localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getProductsBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchProducts();
+});
+
+getProfileBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchProfile();
+});
+
+refreshTokenBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  refreshToken();
+});
